160b323803190ef23b39a0485f484e4636feb401
Joaquin Philco <joaquinphilco@hotmail.com>
2025-01-31 03:41:17 -0500

    media_view: Make sure to always prelaod 2 media objects to the carousel:
    * Create the objects and then move to the desired page.
    
    * Make sure to not add unexistant indeces.
    
    * Init carousel in class init.
    
    Signed-off-by: Joaquin Philco <joaquinphilco@hotmail.com>

--- furios_gallery/media_view.py
+++ furios_gallery/media_view.py
@@ -39,12 +39,18 @@ class MediaView(Adw.NavigationPage):
         self.overlay.set_hexpand(True)
         self.overlay.set_vexpand(True)
 
-        # Carousel
-        self.carousel = self.create_carousel(self.app.current_index)
+        # Create the carousel
+        self.carousel = Adw.Carousel()
+        self.carousel.set_spacing(20)
         self.carousel.set_valign(Gtk.Align.FILL)
         self.carousel.set_halign(Gtk.Align.FILL)
         self.carousel.set_vexpand(True)
         self.carousel.set_hexpand(True)
+        self.carousel.connect("page-changed", self.on_page_changed)
+
+        # Populate the carousel
+        self.populate_carousel(self.carousel, self.app.current_index)
+
         self.main_box.append(self.carousel)
 
         # Add touch event listener
@@ -215,43 +221,48 @@ class MediaView(Adw.NavigationPage):
             new_date = get_file_creation_date(self.app.media_paths[self.app.current_index])
             self.app.header.set_title_widget(Adw.WindowTitle(title=new_date))
 
-    def create_carousel(self, curr_index):
-        carousel = Adw.Carousel()
-        carousel.set_spacing(20)
-        carousel.set_halign(Gtk.Align.CENTER)
-        carousel.set_valign(Gtk.Align.CENTER)
-        carousel.connect("page-changed", self.on_page_changed)
-
-        self.populate_carousel(carousel, curr_index)
-
-        return carousel
-
     def populate_carousel(self, carousel, curr_index):
-        start_index = curr_index
-        end_index = max(curr_index - 5, -1)
+        # 1.- First append the first chil from the first child
+        # 2.- Then determine one of the 3 scenarios:
+        # --> Media index is the first of the album (index = 0)
+        # --> Media index is the last of the album (index = len(self.app.media_paths))
+        # --> Media index is not either of the either cases (being careful with the length of the list)
 
-        for i in range(start_index, end_index, -1):
-            if 0 <= i < len(self.app.media_paths):
-                media_path = self.app.media_paths[i]
-                if media_path.endswith(('.png', '.jpg', '.jpeg', '.gif')):
-                    scrolled_win = Gtk.ScrolledWindow()
-                    scrolled_win.set_hexpand(True)
-                    scrolled_win.set_vexpand(True)
-                    scrolled_win.set_halign(Gtk.Align.FILL)
-                    scrolled_win.set_valign(Gtk.Align.FILL)
-                    zoomable_image = ImageViewerWidget(media_path, self.app, scrolled_win)
-                    zoomable_image.set_vexpand(True)
-                    zoomable_image.set_hexpand(True)
-                    zoomable_image.set_valign(Gtk.Align.CENTER)
-                    zoomable_image.set_halign(Gtk.Align.CENTER)
-                    scrolled_win.set_child(zoomable_image)
-                    zoomable_image.init_gestures()
-                    carousel.append(scrolled_win)
-                elif media_path.endswith(('.mp4', '.mkv', '.avi')):
-                    video_widget = VideoPlayerWidget(media_path)
-                    video_widget.set_halign(Gtk.Align.CENTER)
-                    video_widget.set_valign(Gtk.Align.CENTER)
-                    carousel.append(video_widget)
+        media_path = self.app.media_paths[curr_index]
+        if media_path.endswith(('.png', '.jpg', '.jpeg', '.gif')):
+            scrolled_win = Gtk.ScrolledWindow()
+            scrolled_win.set_hexpand(True)
+            scrolled_win.set_vexpand(True)
+            scrolled_win.set_halign(Gtk.Align.FILL)
+            scrolled_win.set_valign(Gtk.Align.FILL)
+            zoomable_image = ImageViewerWidget(media_path, self.app, scrolled_win)
+            zoomable_image.set_vexpand(True)
+            zoomable_image.set_hexpand(True)
+            zoomable_image.set_valign(Gtk.Align.CENTER)
+            zoomable_image.set_halign(Gtk.Align.CENTER)
+            scrolled_win.set_child(zoomable_image)
+            zoomable_image.init_gestures()
+            carousel.append(scrolled_win)
+        elif media_path.endswith(('.mp4', '.mkv', '.avi')):
+            video_widget = VideoPlayerWidget(media_path)
+            video_widget.set_halign(Gtk.Align.CENTER)
+            video_widget.set_valign(Gtk.Align.CENTER)
+            carousel.append(video_widget)
+
+        if curr_index == 0:
+            # Load the last (if existant) 2 items and preappend to the carousel
+            self.add_media_to_carousel(curr_index + 1, True)
+            self.add_media_to_carousel(curr_index + 2, True)
+        elif curr_index == len(self.app.media_paths) -1:
+            # Load the next (if existant) 2 items and append to the carousel
+            self.add_media_to_carousel(curr_index - 1, False)
+            self.add_media_to_carousel(curr_index - 2, False)
+        else:
+            # Load the next 2 and previous 2 (if existant) items and pre and append them accordingly
+            self.add_media_to_carousel(curr_index + 1, True)
+            self.add_media_to_carousel(curr_index - 1, False)
+            self.add_media_to_carousel(curr_index + 2, True)
+            self.add_media_to_carousel(curr_index - 2, False)
 
     def clear_carousel(self):
         while child := self.carousel.get_first_child():
@@ -266,27 +277,18 @@ class MediaView(Adw.NavigationPage):
 
         if index > self.previous_index:  # Swiping left
             self.app.current_index -= 1
+            if index == carousel.get_n_pages() - 1:
+                self.add_media_to_carousel(self.app.current_index - 1, False)
         elif index < self.previous_index:  # Swiping right
             self.app.current_index += 1
+            if index == carousel.get_n_pages() - 1:
+                self.add_media_to_carousel(self.app.current_index + 1, True)
         else:
             return
 
         self.app.update_properties_view()
 
         self.previous_index = index
-
-        if index == 0:
-            new_start = self.app.current_index + 1
-            new_end = min(self.app.current_index + 4, len(self.app.media_paths) - 1)
-            for i in range(new_start, new_end + 1):
-                if 0 <= i < len(self.app.media_paths):
-                    self.add_media_to_carousel(i, prepend=True)
-        elif index == carousel.get_n_pages() - 1:
-            new_start = self.app.current_index - 1
-            new_end = max(self.app.current_index - 4, 0)
-            for i in range(new_end, new_start + 1):
-                if 0 <= i < len(self.app.media_paths):
-                    self.add_media_to_carousel(i, prepend=False)
 
     def setup_buttons(self):
         buttons_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
@@ -315,14 +317,14 @@ class MediaView(Adw.NavigationPage):
     def update_media_left(self, btn):
         next_position = int(self.carousel.get_position()) - 1
         if next_position >= 0 and self.app.current_index + 1 <= len(self.app.media_paths) - 1:
-            self.carousel.scroll_to(self.carousel.get_nth_page(next_position), True)
             self.on_page_changed(self.carousel, next_position)
+            self.carousel.scroll_to(self.carousel.get_nth_page(next_position), True)
 
     def update_media_right(self, btn):
         next_position = int(self.carousel.get_position()) + 1
         if next_position < len(self.app.media_paths) and next_position < self.carousel.get_n_pages():
-            self.carousel.scroll_to(self.carousel.get_nth_page(next_position), True)
             self.on_page_changed(self.carousel, next_position)
+            self.carousel.scroll_to(self.carousel.get_nth_page(next_position), True)
 
     def add_touch_event_listener(self, widget):
         gesture = Gtk.GestureClick.new()
@@ -335,6 +337,9 @@ class MediaView(Adw.NavigationPage):
             video_widget.on_video_clicked(None)
 
     def add_media_to_carousel(self, index, prepend=False):
+        if index > len(self.app.media_paths) -1 or index < 0:
+            return # Only append if its not out of boundaries
+
         media_path = self.app.media_paths[index]
 
         if media_path.endswith(('.png', '.jpg', '.jpeg', '.gif')):
