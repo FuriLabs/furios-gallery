c399d89eb23079e2e0100d026bd36ad0e8ee8409
Joaquin Philco <joaquinphilco@hotmail.com>
2026-09-22 00:19:14 -0400

    draw_overlay: Make the color chooser a bottom sheet.
    Signed-off-by: Joaquin Philco <joaquinphilco@hotmail.com>

--- furios_gallery/edit_view/ui.py
+++ furios_gallery/edit_view/ui.py
@@ -142,6 +142,103 @@ def make_slider_menu_button(icon_name: str, tooltip: str, title: str, value: flo
 '''
 * Drawing Feature *
 '''
+def apply_color(_btn, chooser, current_color, on_color_changed, dialog):
+    rgba = chooser.get_rgba()
+    current_color.red = rgba.red
+    current_color.green = rgba.green
+    current_color.blue = rgba.blue
+    current_color.alpha = rgba.alpha
+    on_color_changed(rgba)
+    dialog.close()
+
+def color_activated(_chooser, rgba, current_color, on_color_changed, dialog):
+    current_color.red = rgba.red
+    current_color.green = rgba.green
+    current_color.blue = rgba.blue
+    current_color.alpha = rgba.alpha
+    on_color_changed(rgba)
+    dialog.close()
+
+def open_color_sheet(_btn, bar, current_color, on_color_changed):
+    dialog = Adw.Dialog()
+    dialog.set_title("Stroke color")
+    dialog.set_presentation_mode(Adw.DialogPresentationMode.BOTTOM_SHEET)
+    dialog.set_follows_content_size(False)
+
+    root = bar.get_root()
+
+    # Size sheet relative to current window
+    if root is not None:
+        root_height = root.get_allocated_height()
+        root_width = root.get_allocated_width()
+
+        if root_height > 0:
+            dialog.set_content_height(int(root_height / 3))
+        else:
+            dialog.set_content_height(300)
+
+        if root_width > 0:
+            dialog.set_content_width(root_width)
+        else:
+            dialog.set_content_width(360)
+
+    else:
+        dialog.set_content_height(300)
+        dialog.set_content_width(360)
+
+    # Main content
+    content = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=12)
+
+    content.set_margin_top(12)
+    content.set_margin_bottom(12)
+    content.set_margin_start(12)
+    content.set_margin_end(12)
+    content.set_hexpand(True)
+    content.set_vexpand(True)
+
+    # Header
+    header = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=8)
+
+    title = Gtk.Label(label="Stroke color")
+    title.set_halign(Gtk.Align.START)
+    title.set_hexpand(True)
+
+    done_color = Gtk.Button(label="Done")
+    done_color.add_css_class("suggested-action")
+
+    header.append(title)
+    header.append(done_color)
+
+    # Color chooser
+    chooser = Gtk.ColorChooserWidget()
+    chooser.set_rgba(current_color)
+    chooser.set_vexpand(True)
+
+    chooser_scroll = Gtk.ScrolledWindow()
+    chooser_scroll.set_hexpand(True)
+    chooser_scroll.set_vexpand(True)
+    chooser_scroll.set_propagate_natural_width(False)
+    chooser_scroll.set_propagate_natural_height(False)
+    chooser_scroll.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.NEVER)
+    chooser_scroll.set_child(chooser)
+
+    done_color.connect("clicked", apply_color, chooser, current_color, on_color_changed, dialog)
+
+    chooser.connect("color-activated", color_activated, current_color, on_color_changed, dialog)
+
+    # Build sheet
+    content.append(header)
+    content.append(chooser_scroll)
+
+    dialog.set_child(content)
+    dialog.present(bar)
+
+def on_size_value_changed(scale, on_size_changed):
+    on_size_changed(scale.get_value())
+
+def on_undo_pressed(_gesture, _n_press, _x, _y, on_undo):
+    on_undo()
+
 def create_drawing_bar(
     on_cancel,
     on_done,
@@ -160,37 +257,19 @@ def create_drawing_bar(
     # Cancel
     cancel = create_cancel_btn(on_cancel)
 
-    # Color popover
-    color_menu = Gtk.MenuButton()
-    color_menu.set_tooltip_text("Stroke color")
-    color_menu.set_valign(Gtk.Align.CENTER)
-    color_menu.set_has_frame(True)
+    # Color
+    color_btn = Gtk.Button()
+    color_btn.set_tooltip_text("Stroke color")
+    color_btn.set_valign(Gtk.Align.CENTER)
+    color_btn.set_has_frame(True)
 
     color_icon = Gtk.Image.new_from_icon_name("color-select")
     color_icon.set_pixel_size(18)
-    color_menu.set_child(color_icon)
+    color_btn.set_child(color_icon)
 
-    color_pop = Gtk.Popover()
-    color_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=8)
-    for fn in (color_box.set_margin_top, color_box.set_margin_bottom, color_box.set_margin_start, color_box.set_margin_end):
-        fn(10)
+    current_color = initial_rgba.copy()
 
-    color_label = Gtk.Label(label="Stroke color")
-    color_label.set_halign(Gtk.Align.START)
-
-    color_dialog = Gtk.ColorDialog()
-    color_btn = Gtk.ColorDialogButton.new(color_dialog)
-    color_btn.set_rgba(initial_rgba)
-
-    def _on_rgba_notify(btn, _pspec=None):
-        on_color_changed(btn.get_rgba())
-
-    color_btn.connect("notify::rgba", _on_rgba_notify)
-
-    color_box.append(color_label)
-    color_box.append(color_btn)
-    color_pop.set_child(color_box)
-    color_menu.set_popover(color_pop)
+    color_btn.connect("clicked", open_color_sheet, bar, current_color, on_color_changed)
 
     # Size popover
     size_btn = Gtk.MenuButton()
@@ -218,8 +297,8 @@ def create_drawing_bar(
     size_label.add_css_class("dim-label")
 
     low, high = size_range
-    adj = Gtk.Adjustment(value=float(initial_size), lower=float(low), upper=float(high),
-                         step_increment=1.0, page_increment=2.0)
+
+    adj = Gtk.Adjustment(value=float(initial_size), lower=float(low), upper=float(high), step_increment=1.0, page_increment=2.0)
 
     scale = Gtk.Scale(orientation=Gtk.Orientation.HORIZONTAL, adjustment=adj)
     scale.set_draw_value(True)
@@ -228,10 +307,7 @@ def create_drawing_bar(
     scale.set_hexpand(True)
     scale.set_size_request(150, -1)
 
-    def _on_size_value_changed(s):
-        on_size_changed(s.get_value())
-
-    scale.connect("value-changed", _on_size_value_changed)
+    scale.connect("value-changed", on_size_value_changed, on_size_changed)
 
     size_box.append(size_label)
     size_box.append(scale)
@@ -249,7 +325,7 @@ def create_drawing_bar(
     undo_btn.set_child(undo_icon)
 
     gesture = Gtk.GestureClick.new()
-    gesture.connect("pressed", lambda *_: on_undo())
+    gesture.connect("pressed", on_undo_pressed, on_undo)
     undo_btn.add_controller(gesture)
 
     # Done
@@ -262,7 +338,7 @@ def create_drawing_bar(
     # Layout
     bar.append(cancel)
     bar.append(Gtk.Separator(orientation=Gtk.Orientation.VERTICAL))
-    bar.append(color_menu)
+    bar.append(color_btn)
     bar.append(Gtk.Separator(orientation=Gtk.Orientation.VERTICAL))
     bar.append(size_btn)
     bar.append(Gtk.Separator(orientation=Gtk.Orientation.VERTICAL))
